84675559628c7e8a2e2f878c5e45f0cc1c9964ed
Mhaneefa93 <haneefchani@gmail.com>
2025-10-05 11:59:07 +0000

    test

--- python-app/app.py
+++ python-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask App! testing argocd"
+    return "Hello from Flask App using ArgoCD"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
