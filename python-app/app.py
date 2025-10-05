a10932358f3cc13bba7927a6ee32b39b43df34eb
Mhaneefa93 <haneefchani@gmail.com>
2025-10-05 11:49:00 +0000

    test

--- python-app/app.py
+++ python-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask App! my new version released"
+    return "Hello from Flask App! testing argocd"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
